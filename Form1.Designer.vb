cb1df4f82890322e0c34b4801e02d3f11a25bccc
Tess <dublowduck@gmail.com>
2022-02-16 13:11:46 +0000

    Rewrote all game code. Basic board setup for both players
    Rewrote all of the game code. Basic board setup for both players, computers ship locations are hard coded in. Added messages written to the debug window (Needs improving)

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,23 +22,16 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.lblGamePlaceholder = New System.Windows.Forms.Label()
         Me.btnForm1Exit = New System.Windows.Forms.Button()
         Me.menuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.toolStripTools = New System.Windows.Forms.ToolStripMenuItem()
         Me.developerSettingsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lsbPlayerGuessBoard = New System.Windows.Forms.ListBox()
+        Me.lsbPlayerGameBoard = New System.Windows.Forms.ListBox()
+        Me.lblPlayerGuessBoard = New System.Windows.Forms.Label()
+        Me.lblPlayerGameBoard = New System.Windows.Forms.Label()
         Me.menuStrip1.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'lblGamePlaceholder
-        '
-        Me.lblGamePlaceholder.AutoSize = True
-        Me.lblGamePlaceholder.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.lblGamePlaceholder.Location = New System.Drawing.Point(250, 208)
-        Me.lblGamePlaceholder.Name = "lblGamePlaceholder"
-        Me.lblGamePlaceholder.Size = New System.Drawing.Size(273, 37)
-        Me.lblGamePlaceholder.TabIndex = 0
-        Me.lblGamePlaceholder.Text = "Placeholder for Game"
         '
         'btnForm1Exit
         '
@@ -71,14 +64,55 @@ Partial Class Form1
         Me.developerSettingsToolStripMenuItem.Size = New System.Drawing.Size(172, 22)
         Me.developerSettingsToolStripMenuItem.Text = "Developer Settings"
         '
+        'lsbPlayerGuessBoard
+        '
+        Me.lsbPlayerGuessBoard.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lsbPlayerGuessBoard.FormattingEnabled = True
+        Me.lsbPlayerGuessBoard.ItemHeight = 21
+        Me.lsbPlayerGuessBoard.Location = New System.Drawing.Point(41, 62)
+        Me.lsbPlayerGuessBoard.Name = "lsbPlayerGuessBoard"
+        Me.lsbPlayerGuessBoard.Size = New System.Drawing.Size(180, 214)
+        Me.lsbPlayerGuessBoard.TabIndex = 3
+        '
+        'lsbPlayerGameBoard
+        '
+        Me.lsbPlayerGameBoard.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lsbPlayerGameBoard.FormattingEnabled = True
+        Me.lsbPlayerGameBoard.ItemHeight = 21
+        Me.lsbPlayerGameBoard.Location = New System.Drawing.Point(372, 62)
+        Me.lsbPlayerGameBoard.Name = "lsbPlayerGameBoard"
+        Me.lsbPlayerGameBoard.Size = New System.Drawing.Size(180, 214)
+        Me.lsbPlayerGameBoard.TabIndex = 4
+        '
+        'lblPlayerGuessBoard
+        '
+        Me.lblPlayerGuessBoard.AutoSize = True
+        Me.lblPlayerGuessBoard.Location = New System.Drawing.Point(73, 44)
+        Me.lblPlayerGuessBoard.Name = "lblPlayerGuessBoard"
+        Me.lblPlayerGuessBoard.Size = New System.Drawing.Size(107, 15)
+        Me.lblPlayerGuessBoard.TabIndex = 5
+        Me.lblPlayerGuessBoard.Text = "Player Guess Board"
+        '
+        'lblPlayerGameBoard
+        '
+        Me.lblPlayerGameBoard.AutoSize = True
+        Me.lblPlayerGameBoard.Location = New System.Drawing.Point(412, 44)
+        Me.lblPlayerGameBoard.Name = "lblPlayerGameBoard"
+        Me.lblPlayerGameBoard.Size = New System.Drawing.Size(107, 15)
+        Me.lblPlayerGameBoard.TabIndex = 6
+        Me.lblPlayerGameBoard.Text = "Player Game Board"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblPlayerGameBoard)
+        Me.Controls.Add(Me.lblPlayerGuessBoard)
+        Me.Controls.Add(Me.lsbPlayerGameBoard)
+        Me.Controls.Add(Me.lsbPlayerGuessBoard)
         Me.Controls.Add(Me.menuStrip1)
         Me.Controls.Add(Me.btnForm1Exit)
-        Me.Controls.Add(Me.lblGamePlaceholder)
         Me.MainMenuStrip = Me.menuStrip1
         Me.Name = "Form1"
         Me.Text = "VB-Warships"
@@ -88,10 +122,12 @@ Partial Class Form1
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents lblGamePlaceholder As Label
     Friend WithEvents btnForm1Exit As Button
     Friend WithEvents menuStrip1 As MenuStrip
     Friend WithEvents toolStripTools As ToolStripMenuItem
     Friend WithEvents developerSettingsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents lsbPlayerGuessBoard As ListBox
+    Friend WithEvents lsbPlayerGameBoard As ListBox
+    Friend WithEvents lblPlayerGuessBoard As Label
+    Friend WithEvents lblPlayerGameBoard As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,39 +1,145 @@
 ﻿Public Class Form1
 
-    Private Sub readyPlayerComputer()
-        'get the computer player ready
+    Private Sub readyPlayer1()
+        'load the arrays for the players boards, fill them with water and print them to list boxes
 
-        'numbers for all the different ships on the board + water
-        '0 = water
-        '1 = destroyer (2 holes)
-        '2 = submarine (3 holes)
-        '3 = cruiser (3 holes)
-        '4 = battleship (4 holes)
-        '5 = carrier (5 holes)
+        System.Diagnostics.Debug.WriteLine("readyPlayer1")
 
-        Dim computerGameBoard(9, 9) As Integer 'board for the positions of comp's ships
-
+        'Step 1: create player game array
+        Dim arrayPlayerGameBoard(9, 9) As Integer 'Create board for the positions of the players ships
         'set all squares of the game board to 0 (water)
-        For x = 0 To UBound(computerGameBoard) 'for every column  x
-            For y = 0 To UBound(computerGameBoard) 'for every row y
-                computerGameBoard(0, y) = 0 'set selected cell in row y to 0
+        For x = 0 To UBound(arrayPlayerGameBoard) 'for every column  x
+            For y = 0 To UBound(arrayPlayerGameBoard) 'for every row y
+                arrayPlayerGameBoard(0, y) = 0 'set selected cell in row y to 0
             Next
-            computerGameBoard(x, 0) = 0 'set selected cell in column  x to 0
+            arrayPlayerGameBoard(x, 0) = 0 'set selected cell in column  x to 0
         Next
 
+        'print player game array to lsbPlayerGameBoard
+        System.Diagnostics.Debug.WriteLine("readyPlayer1: add 'arrayPlayerGameBoard' to 'lsbPlayerGameBoard'")
+        Dim rows(9) As String
+        lsbPlayerGuessBoard.Items.Clear()
+        For r = 0 To 9
+            For c = 0 To 9
+                rows(r) &= arrayPlayerGameBoard(r, c)
+                If c < 9 Then
+                    rows(r) &= " " 'this puts a space between each item horizontally
+                End If
+            Next
+            lsbPlayerGameBoard.Items.Add(rows(r))
+        Next
+
+        ' Step 2: load player guess array
+        Dim arrayPlayerGuessBoard(9, 9) As Integer 'board for the positions of the players guesses
+
+        'set all squares of the guess board to 0 (water)
+        For x = 0 To UBound(arrayPlayerGuessBoard) 'for every column  x
+            For y = 0 To UBound(arrayPlayerGuessBoard) 'for every row y
+                arrayPlayerGuessBoard(x, y) = 0 'set selected cell in row y to 0
+            Next
+            arrayPlayerGuessBoard(x, 0) = 0 'set selected cell in column  x to 0
+        Next
+
+        'print player guess array to lsbPlayerGuessBoard
+        System.Diagnostics.Debug.WriteLine("readyPlayer1: add 'arrayPlayerGuessBoard' to 'lsbPlayerGuessBoard'")
+        Dim rows2(9) As String
+        lsbPlayerGuessBoard.Items.Clear()
+        For r = 0 To 9
+            For c = 0 To 9
+                rows2(r) &= arrayPlayerGuessBoard(r, c)
+                If c < 9 Then
+                    rows2(r) &= " " 'this puts a space between each item horizontally
+                End If
+            Next
+            lsbPlayerGuessBoard.Items.Add(rows2(r))
+        Next
+
+        System.Diagnostics.Debug.WriteLine("Exit: readyPlayer1")
     End Sub
 
-    Private Sub readyPlayer1()
-        'TODO: Make this sub
+    Public Sub readyPlayerComputer()
+        'load the arrays for the computers boards, fill them with water and then add ships from hard coded layout
+
+        System.Diagnostics.Debug.WriteLine("readyPlayerComputer")
+        ' meaning of all values in the array
+        ' 0 = water
+        ' 1 = destroyer (2 holes)
+        ' 2 = cruiser (3 holes)
+        ' 3 = submarine (3 holes)
+        ' 4 = battleship (4 holes)
+        ' 5 = aircraft carrier (5 holes)
+
+        ' Step 1: load computer game array
+        Dim arrayComputerGameBoard(9, 9) As Integer 'Create board for the positions of the players ships
+
+        'set all squares of the game board to 0 (water)
+        For x = 0 To UBound(arrayComputerGameBoard) 'for every column  x
+            For y = 0 To UBound(arrayComputerGameBoard) 'for every row y
+                arrayComputerGameBoard(0, y) = 0 'set selected cell in row y to 0
+            Next
+            arrayComputerGameBoard(x, 0) = 0 'set selected cell in column  x to 0
+        Next
+
+        'Hard coded layout:
+        '    0 1 2 3 4 5 6 7 8 9
+
+        '0:  0 3 3 3 0 0 0 0 0 0
+        '1:  0 0 0 0 0 0 0 0 0 0
+        '2:  0 5 5 5 5 5 0 0 0 0
+        '3:  0 0 0 0 0 0 0 2 2 2
+        '4:  0 0 1 0 0 0 0 0 0 0
+        '5:  0 0 1 4 0 0 0 0 0 0
+        '6:  0 0 0 4 0 0 0 0 0 0
+        '7:  0 0 0 4 0 0 0 0 0 0
+        '8:  0 0 0 4 0 0 0 0 0 0
+        '9:  0 0 0 0 0 0 0 0 0 0
+
+        'add the ships from the hard coded layout above
+        System.Diagnostics.Debug.WriteLine("readyPlayerComputer: add ships to Computer game board")
+        arrayComputerGameBoard(0, 1) = 3
+        arrayComputerGameBoard(0, 2) = 3
+        arrayComputerGameBoard(0, 3) = 3
+        arrayComputerGameBoard(2, 1) = 5
+        arrayComputerGameBoard(2, 2) = 5
+        arrayComputerGameBoard(2, 3) = 5
+        arrayComputerGameBoard(2, 4) = 5
+        arrayComputerGameBoard(2, 5) = 5
+        arrayComputerGameBoard(3, 7) = 2
+        arrayComputerGameBoard(3, 8) = 2
+        arrayComputerGameBoard(3, 9) = 2
+        arrayComputerGameBoard(4, 2) = 1
+        arrayComputerGameBoard(5, 2) = 1
+        arrayComputerGameBoard(3, 5) = 4
+        arrayComputerGameBoard(3, 6) = 4
+        arrayComputerGameBoard(3, 7) = 4
+        arrayComputerGameBoard(3, 8) = 4
+
+        ' Step 2: load computer guess array
+        Dim arrayComputerGuessBoard(9, 9) As Integer 'board for the positions of the players guesses
+
+        'set all squares of the guess array to 0 (water)
+        For x = 0 To UBound(arrayComputerGuessBoard) 'for every column  x
+            For y = 0 To UBound(arrayComputerGuessBoard) 'for every row y
+                arrayComputerGuessBoard(x, y) = 0 'set selected cell in row y to 0
+            Next
+            arrayComputerGuessBoard(x, 0) = 0 'set selected cell in column  x to 0
+        Next
+
+        System.Diagnostics.Debug.WriteLine("Exit: readyPlayerComputer")
     End Sub
 
     Private Sub loadGame()
+        System.Diagnostics.Debug.WriteLine("loadGame:")
+        System.Diagnostics.Debug.WriteLine("Call: readyPlayer1")
+        readyPlayer1() '(Yes I did Structure this program deliberately so I could write Ready Player 1)
+        System.Diagnostics.Debug.WriteLine("Call: readyPlayerComputer")
         readyPlayerComputer()
-        readyPlayer1() 'currently does nothing
-        '(Yes I did Structure this program deliberately so I could write Ready Player 1)
+
+        System.Diagnostics.Debug.WriteLine("Exit: loadGame")
     End Sub
 
     Private Sub btnForm1Exit_Click(sender As Object, e As EventArgs) Handles btnForm1Exit.Click
+        System.Diagnostics.Debug.WriteLine("EXIT GAME")
         Me.Close() 'close the game
     End Sub
 
@@ -41,8 +147,13 @@
         devSettings.Show() 'open the developer settings window
     End Sub
 
-    Private Sub btnLoadGame_Click(sender As Object, e As EventArgs)
-        loadGame()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        System.Diagnostics.Debug.WriteLine(vbNewLine)
+        System.Diagnostics.Debug.WriteLine("Done!")
+        System.Diagnostics.Debug.WriteLine("Form1_Load")
+        System.Diagnostics.Debug.WriteLine("Call: loadGame()")
+        loadGame() 'start the game
+        System.Diagnostics.Debug.WriteLine("Exit: Form1_load")
     End Sub
 
 End Class
